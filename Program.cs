aa74bf6735e9616ce363a8af98ca3886dd82f9bc
unknown <nkosimova06@gmail.com>
2020-04-13 20:55:06 +0500

    creating class Student with necessary fields

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        class Student
+        {
+            public int id{get;}
+            public string FirstName{get; set;}
+            public string LastName{get; set;}
+            public string MiddleName{get; set;}
+            public string BirthYear{get; set;}
+            public int StudId{get; set;}
+
+            public int CourseId{get; set;}
+            public string Course{get; set;}
+
+
+        }
+    }
+}
